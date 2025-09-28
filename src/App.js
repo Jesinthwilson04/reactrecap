cc3845d9862358a05d5e27b845bfe6ea35a5f641
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-09-28 21:51:49 +0530

    buttonlog

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
 import './App.css';
 
 
-const name="Jesinth"
-  
-// //function component
-// function Button(){
-//   const buttonname="clickMe"
-//   return (
-//     <button onClick={
-//       () => console.log("Button Clicked")
-//     }> {buttonname}</button>
-//   )
-// }
-//search component
-const Search=()=>{
-  return(
-    <input type="text" placeholder="Search here"/>
-)
-}
-function Header(){
-  return(
-    <div className='header' style={{
-      backgroundColor: 'grey',
-      display: 'flex',
-      flex: '1 0 auto'
-    }}>
-    <h1> Header</h1>
-    <Search/>
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-    </div>
-
-  )
-}
 
 function App() {
+  let count=0;
+  const onclickLog=()=>{
+    count++
+    console.log(`button clicked ${count} times`)
+  }
   return (
     <div className="App">
-      <Header/>
-      <h1>Hey {name} !!</h1>
-      {/* <Button/>
-      <Search/> */}
+      <button onClick={onclickLog}>clickme</button>
     </div>
   );
 }
