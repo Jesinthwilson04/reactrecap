f16d6f017511b8d06cea6d0d5fdc3666df680a92
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-09-28 21:11:44 +0530

    head

--- src/App.js
+++ src/App.js
@@ -3,26 +3,43 @@ import './App.css';
 
 const name="Jesinth"
   
-//function component
-function Button(){
-  const buttonname="clickMe"
-  return (
-    <button onClick={
-      () => console.log("Button Clicked")
-    }> {buttonname}</button>
-  )
-}
+// //function component
+// function Button(){
+//   const buttonname="clickMe"
+//   return (
+//     <button onClick={
+//       () => console.log("Button Clicked")
+//     }> {buttonname}</button>
+//   )
+// }
+//search component
 const Search=()=>{
   return(
     <input type="text" placeholder="Search here"/>
 )
 }
+function Header(){
+  return(
+    <>
+    <h1> Header</h1>
+    <Search/>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+    </>
+
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <h1>Hey {name} !!</h1>
-      <Button/>
-      <Search/>
+      <Header/>
+      {/* <Button/>
+      <Search/> */}
     </div>
   );
 }
