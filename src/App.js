fdb9dabea803797344ebae71da3495416c4481b5
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-09-28 22:24:41 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+//condtional rendering
+function ConditionalRender(){
+  const isAdmin=true;
+  return(
+    <div>
+      {isAdmin ? <h1>Admin</h1> : <h1>User</h1>}
+    </div>
+  )
+}
 
-
-
+//count event handler
 function App() {
   let count=0;
   const onclickLog=(event)=>{
@@ -9,6 +17,7 @@ function App() {
     console.log(`button clicked ${count} times`)
     console.log(event.target.value)
   }
+  //input event handler
   function onSearchChange(event){
     if(event.target.value.length>50)
       {
@@ -22,6 +31,7 @@ function App() {
     <div className="App">
       <button onClick={onclickLog} value={0}>clickme</button>
       <input type="text" onChange={onSearchChange} placeholder='search here'/>
+      <ConditionalRender/>
     </div>
   );
 }
