d7f084b07f269abb74534abb024cd76bcbcea29e
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-09-28 22:01:34 +0530

    search

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import './App.css';
 
 function App() {
   let count=0;
-  const onclickLog=()=>{
+  const onclickLog=(event)=>{
     count++
     console.log(`button clicked ${count} times`)
+    console.log(event.target.value)
+  }
+  function onSearchChange(event){
+    if(event.target.value.length>50)
+      {
+        event.target.value="exceeded the limit"
+      }
+
+    
+    console.log(event.target.value.length)
   }
   return (
     <div className="App">
-      <button onClick={onclickLog}>clickme</button>
+      <button onClick={onclickLog} value={0}>clickme</button>
+      <input type="text" onChange={onSearchChange} placeholder='search here'/>
     </div>
   );
 }
