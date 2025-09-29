a5aa6ab3e5a4eaccc56a1de7ca03efdf02dead34
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-09-29 09:46:55 +0530

    maprendering

--- src/App.js
+++ src/App.js
@@ -9,29 +9,53 @@ function ConditionalRender(){
   )
 }
 
-//count event handler
-function App() {
-  let count=0;
-  const onclickLog=(event)=>{
-    count++
-    console.log(`button clicked ${count} times`)
-    console.log(event.target.value)
-  }
-  //input event handler
-  function onSearchChange(event){
-    if(event.target.value.length>50)
-      {
-        event.target.value="exceeded the limit"
-      }
+//input event handler
+function onSearchChange(event){
+  if(event.target.value.length>50)
+    {
+      event.target.value="exceeded the limit"
+    }
 
-    
-    console.log(event.target.value.length)
-  }
+  
+  console.log(event.target.value.length)
+}
+
+
+//count event handler
+const onclickLog=(event)=>{
+  let count=0;
+  count++
+  console.log(`button clicked ${count} times`)
+  console.log(event.target.value)
+}
+
+
+function List(){
+  const names=['jesi','kann','wila'];
+
+  return (
+    <>
+    <ul>
+      {names.map(name=> <li key={name}> {name}</li>)}
+    </ul>
+    <p>
+      {names.map((name)=> name)}
+      {names.map((name,index)=> index===2 ? <strong> {name}</strong> : <>{name}</>)}
+    </p>
+    </>
+
+
+  )
+}
+
+
+function App() {
   return (
     <div className="App">
       <button onClick={onclickLog} value={0}>clickme</button>
       <input type="text" onChange={onSearchChange} placeholder='search here'/>
       <ConditionalRender/>
+      <List/>
     </div>
   );
 }
